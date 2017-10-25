265bbb134c9b68cc50e3deca9e6bf49470df967c
sharad joe <sharadjoe48@gmail.com>
2017-10-25 12:55:46 +0530

    Created a sample model

---
+++ Desktop/backend/user/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class consumers(models.Model):
+    user_name=models.CharField(max_length=50)
+    user_first_name=models.CharField(max_length=50)
+    user_last_name=models.CharField(max_length=50)
+    user_email=models.CharField(max_length=50)
