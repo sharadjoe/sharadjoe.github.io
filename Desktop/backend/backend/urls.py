fa086e76a4c9e471e60082d4fe92fb81a3f9fdbd
sharad joe <sharadjoe48@gmail.com>
2017-10-25 08:33:47 +0530

    Fiqured out the stupid error and also has provided the views and url

--- Desktop/backend/backend/urls.py
+++ Desktop/backend/backend/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from .import views
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^home/', views.home),
 ]
---
+++ Desktop/backend/backend/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from books.models import bookly
+
+def home(request):
+    return render(request, "homep.html")
