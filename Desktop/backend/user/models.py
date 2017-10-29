dc42b995e4dd1d248a6e801f502d84f12d2b6fc5
sharad joe <sharadjoe48@gmail.com>
2017-10-29 23:38:51 +0530

    test commit

--- Desktop/backend/user/models.py
+++ Desktop/backend/user/models.py
@@ -5,4 +5,5 @@ from django.db import models
 class consumers(models.Model):
     user_name=models.CharField(max_length=50)
     user_first_name=models.CharField(max_length=50)
+    user_last_name=models.CharField(max_length=50)
     user_email=models.CharField(max_length=50)
