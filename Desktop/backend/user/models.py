ac0cc87c4155d9a4c5642060e8031785beda140f
sharad joe <sharadjoe48@gmail.com>
2017-10-30 21:24:23 +0530

    added semester to model

--- Desktop/backend/user/models.py
+++ Desktop/backend/user/models.py
@@ -7,3 +7,4 @@ class consumers(models.Model):
     user_first_name=models.CharField(max_length=50)
     user_last_name=models.CharField(max_length=50)
     user_email=models.CharField(max_length=50)
+    user_semester=models.IntegerField()
