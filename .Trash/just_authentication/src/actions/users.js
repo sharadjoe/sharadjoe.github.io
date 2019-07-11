b2b784ffec3147686afd7a6c3cf900f450377d98
Chachu <sharadjoe48@gmail.com>
2019-07-11 09:41:25 +0530

    some remodifying

--- .Trash/just_authentication/src/actions/users.js
+++
@@ -1,9 +0,0 @@
-import api from '../api';
-
-import { userLoggedIn } from './auth';
-
-export const signup = data => dispatch =>
-  api.user.signup(data).then(user => {
-    localStorage.theJWT = user.token;
-    dispatch(userLoggedIn(user));
-  });
